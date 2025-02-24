2b5970eb1add2e1c50c8fb765c2d9863f47cef04
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2025-02-25 11:17:32 +1300

    Movement, Camera
    Added cinnemachine so the camera moves smoothly and properly around the player, also tracks the player when moving. Basic cardinal direction movements also added

--- TheThread/Assets/Scripts/PlayerMovement.cs
+++ TheThread/Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PlayerMovement : MonoBehaviour{
+    public CharacterController controller;
+    public Transform cam;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public float speed = 10f;
+    public float smoothTurnTime = 0.1f;
+    float smoothTurnSpeed;
+
+    void Update(){
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude > 0.1f){
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothTurnSpeed, smoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
     }
 }
