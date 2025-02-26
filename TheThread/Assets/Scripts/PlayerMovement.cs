34954f558aca8d1d4f8bf4aa933f5d609141676f
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2025-02-27 11:57:27 +1300

    Grappling

--- TheThread/Assets/Scripts/PlayerMovement.cs
+++ TheThread/Assets/Scripts/PlayerMovement.cs
@@ -6,11 +6,27 @@ public class PlayerMovement : MonoBehaviour{
     public CharacterController controller;
     public Transform cam;
 
+    //Moving around
     public float speed = 10f;
     public float smoothTurnTime = 0.1f;
     float smoothTurnSpeed;
 
+    //Jumping
+    float jumpStrength = 2f;
+    public float gravity = -9.81f;
+    Vector3 velocity;
+    bool isGrounded;
+
+    private void Start(){
+        Cursor.visible = false;
+    }
+
     void Update(){
+        isGrounded = controller.isGrounded;
+        if (isGrounded) {
+            velocity.y = -2f;
+        }
+
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
@@ -23,5 +39,12 @@ public class PlayerMovement : MonoBehaviour{
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+
+        if (Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(jumpStrength * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
