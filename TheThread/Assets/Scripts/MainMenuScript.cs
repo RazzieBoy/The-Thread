5d246e1b0f40986d83936c94fecb5e64588521d8
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2025-05-29 08:32:14 +1200

    UI

--- TheThread/Assets/Scripts/MainMenuScript.cs
+++ TheThread/Assets/Scripts/MainMenuScript.cs
@@ -1,31 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class MainMenuScript : MonoBehaviour{
+public class MainMenuScript : MonoBehaviour
+{
     public GameObject mainMenu;
     public GameObject gameMode;
     public GameObject settingsMenu;
-
-    public void PlayButton(){
-        mainMenu.SetActive(false);
-        gameMode.SetActive(true);
-    }
-
-    public void SettingsButton(){
-        mainMenu.SetActive(false);
-        settingsMenu.SetActive(true);
-    }
-
-    public void QuitButtonn(){
-        Application.Quit();
-    }
 
     private void Start()
     {
         mainMenu.SetActive(true);
         gameMode.SetActive(false);
         settingsMenu.SetActive(false);
+    }
+
+    public void PlayButton()
+    {
+        mainMenu.SetActive(false);
+        gameMode.SetActive(true);
+    }
+
+    public void SettingsButton()
+    {
+        mainMenu.SetActive(false);
+        settingsMenu.SetActive(true);
+    }
+
+    public void QuitButtonn()
+    {
+        Application.Quit();
+    }
+
+    public void LoadSceneByName()
+    {
+        SceneManager.LoadScene("Scenes/MainMenu");
+    }
+
+    public void LoadNextInBuild()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BackButton()
+    {
+        mainMenu.SetActive(true);
+        settingsMenu.SetActive(false);
+        gameMode.SetActive(false);
     }
 
 }
