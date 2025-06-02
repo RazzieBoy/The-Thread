4b71419ed9036847972c1b150e4075ffe571eb1e
RazzieBoy <97880341+RazzieBoy@users.noreply.github.com>
2025-06-03 09:50:26 +1200

    Player Prefab Added

--- TheThread/Assets/Scripts/Testing/MoveCameraScript.cs
+++ TheThread/Assets/Scripts/Testing/MoveCameraScript.cs
@@ -2,13 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCameraScript : MonoBehaviour
-{
+public class MoveCameraScript : MonoBehaviour{
     public Transform cameraPosition;
 
     // Update is called once per frame
-    void Update()
-    {
+
+    private void Start(){
+        //cameraPosition = FindFirstObjectByType<GameObject>
+    }
+
+    void Update(){
         transform.position = cameraPosition.position;
     }
 }
